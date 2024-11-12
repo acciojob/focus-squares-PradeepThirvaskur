e051dea104adcf1b5d5157056215ca253a1179f4
pradeep thirvaskur <142670238+PradeepThirvaskur@users.noreply.github.com>
2024-11-12 12:26:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+let div2= document.querySelector("#square2");
+let div3= document.querySelector("#square3");
+let div1= document.querySelector("#square1");
+
+function color1(){
+    div2.style.backgroundColor='#6F4E37';
+    div3.style.backgroundColor='#6F4E37';
+    div1.style.backgroundColor='#E6E6FA'
+}
+function color2(){
+    div1.style.backgroundColor='#6F4E37';
+    div3.style.backgroundColor='#6F4E37';
+    div2.style.backgroundColor='#E6E6FA';
+}
+function color3(){
+    div1.style.backgroundColor='#6F4E37';
+    div2.style.backgroundColor='#6F4E37';
+    div3.style.backgroundColor='#E6E6FA';
+}
