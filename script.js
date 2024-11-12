8377f18b2398d1dfe2f48d0e9c1804e6598b807f
pradeep thirvaskur <142670238+PradeepThirvaskur@users.noreply.github.com>
2024-11-12 12:35:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
-//your JS code here. If required.
-let div2= document.querySelector("#square2");
-let div3= document.querySelector("#square3");
-let div1= document.querySelector("#square1");
+let div1 = document.querySelector("#square1");
+let div2 = document.querySelector("#square2");
+let div3 = document.querySelector("#square3");
 
-function color1(){
-    div2.style.backgroundColor='#6F4E37';
-    div3.style.backgroundColor='#6F4E37';
-    div1.style.backgroundColor='#E6E6FA'
-}
-function color2(){
-    div1.style.backgroundColor='#6F4E37';
-    div3.style.backgroundColor='#6F4E37';
-    div2.style.backgroundColor='#E6E6FA';
-}
-function color3(){
-    div1.style.backgroundColor='#6F4E37';
-    div2.style.backgroundColor='#6F4E37';
-    div3.style.backgroundColor='#E6E6FA';
-}
+div1.addEventListener('mouseover', () => {
+    div2.style.backgroundColor = '#6F4E37';
+    div3.style.backgroundColor = '#6F4E37';
+});
+div1.addEventListener('mouseout', () => {
+    div2.style.backgroundColor = '#E6E6FA';
+    div3.style.backgroundColor = '#E6E6FA';
+});
+
+div2.addEventListener('mouseover', () => {
+    div1.style.backgroundColor = '#6F4E37';
+    div3.style.backgroundColor = '#6F4E37';
+});
+div2.addEventListener('mouseout', () => {
+    div1.style.backgroundColor = '#E6E6FA';
+    div3.style.backgroundColor = '#E6E6FA';
+});
+
+
+div3.addEventListener('mouseover', () => {
+    div1.style.backgroundColor = '#6F4E37';
+    div2.style.backgroundColor = '#6F4E37';
+});
+div3.addEventListener('mouseout', () => {
+    div2.style.backgroundColor = '#E6E6FA';
+    div1.style.backgroundColor = '#E6E6FA';
+});
